c9d30ea914f4805fb912e01258d4ed0cbed1112b
Micha <micha-elmans@outlook.com>
2024-11-27 14:26:42 +0100

    made the sections migration

--- database/migrations/2024_11_27_124604_create_sections_table.php
+++ database/migrations/2024_11_27_124604_create_sections_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('sections', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('permissions_id')->constrained();
+            $table->string('name', 50);
             $table->timestamps();
         });
     }
