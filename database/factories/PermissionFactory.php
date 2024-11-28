b988d66fe0904a4c827c4df91f0a8b7cbe148da4
Micha <micha-elmans@outlook.com>
2024-11-28 09:53:10 +0100

    fix a spellings mistake

--- database/factories/PermissionFactory.php
+++ database/factories/PermissionFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Permission>
  */
-class PremissionFactory extends Factory
+class PermissionFactory extends Factory
 {
     /**
      * Define the model's default state.
