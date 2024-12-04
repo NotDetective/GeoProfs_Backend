d1198b44ecdb30f3e1b98004f60ac1ebede02345
Micha <micha-elmans@outlook.com>
2024-12-04 14:43:10 +0100

    added some commands and fix that the logout you need to be login in

--- routes/api.php
+++ routes/api.php
@@ -10,5 +10,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['api'] ], function () {
     Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-    Route::delete('/logout', [AuthenticatedSessionController::class, 'destroy']);
+    // here need to go all API routes that do not require the user to be authenticated(logged in)
+
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+
+        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+
+        // here need to go all API routes that require the user to be authenticated(logged in)
+
+    });
+
 });
