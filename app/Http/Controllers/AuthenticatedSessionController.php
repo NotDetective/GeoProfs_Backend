cac640a9810c53093757d910930f91dd1b484589
Micha <micha-elmans@outlook.com>
2024-12-11 16:47:30 +0100

    added docs for the login request

--- app/Http/Controllers/AuthenticatedSessionController.php
+++ app/Http/Controllers/AuthenticatedSessionController.php
@@ -6,12 +6,26 @@ use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use OpenApi\Attributes as OA;
 
 class AuthenticatedSessionController extends Controller
 {
-    /**
-     * Handle an incoming authentication request.
-     */
+
+    #[OA\Post(path: '/login', summary: 'Create a new session.', tags: ['Authentication'])]
+    #[OA\RequestBody(description: 'User credentials.', required: true, content: [
+        new OA\JsonContent(
+            required: ['email', 'password'],
+            properties: [
+                new OA\Property(property: 'email', type: 'string', example: 'user@app.com'),
+                new OA\Property(property: 'password', type: 'string', example: 'password'),
+            ],
+            type: 'object',
+        ),
+    ])]
+    #[OA\Response(response: '201', description: 'Session created.', content: new OA\JsonContent(properties: [
+            new OA\Property(property: 'token', type: 'string', example: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJlbWFpbCI6InVzZXJAYXBwLmNvbSIsImV4cCI6MTYyNjQwNjYwNn0.')
+        ]))]
+    #[OA\Response(response: '422', description: 'Invalid credentials.')]
     public function store(LoginRequest $request): Response
     {
         $request->authenticate();
